d2d61356de127b211eed2f9e335061dc5b256d90
david seol <dseol@davids-air-2.lan>
2026-06-21 23:59:37 -0400

    Replace Microscopy description with detailed schedule

--- src/routes/Microscopy.jsx
+++ src/routes/Microscopy.jsx
@@ -14,10 +14,35 @@ export default function Microscopy() {
           <div className={styles.serviceCard}>
             <div className={styles.serviceBody}>
               <div className={styles.divider} />
-              <p className={styles.serviceDesc}>
-                This hands-on workshop introduces middle school students to microscopy through activities that relate to real world application. Students will learn the parts of a microscope, how to use a microscope, and how to identify organisms from their own local freshwater ponds. No prior experience is needed. However, students will be asked to bring a small sample from their own local freshwater ponds that includes sediment, water, and aquatic plant life.
-              </p>
+              <div className={styles.divider} />
               <h2>Schedule</h2>
+              <div className={styles.scheduleSection}>
+                <h3>(30 min) Microscopy Overview & Anatomy</h3>
+                <ul>
+                  <li>What is microscopy and why do we use it?</li>
+                  <li>Microscope parts and demo (Eyepiece, stage, focus knobs, objective lenses)</li>
+                  <li>Rules of using a microscope</li>
+                </ul>
+              </div>
+              <div className={styles.scheduleSection}>
+                <h3>(45 min) Known Slide Walkthrough</h3>
+                <ul>
+                  <li>Handing out prepared slide sets/allow students to pick out slides, and hand out student lab sheets</li>
+                  <li>Walkthrough of adjusting focus step by step</li>
+                  <li>Observing and sketching three different slides</li>
+                </ul>
+              </div>
+              <div className={styles.scheduleSection}>
+                <h3>(15 min) BREAK</h3>
+              </div>
+              <div className={styles.scheduleSection}>
+                <h3>(1.5 hr) Student Design Activity</h3>
+                <ul>
+                  <li>Instructor walkthrough: How to make a wet mount slide using a plastic dropper and coverslip without trapping bubbles</li>
+                  <li>Students set up slides/stations to find organisms within the slide</li>
+                  <li>Using sketches/observations, students will use reference guide to identify organisms and determine whether or not water source is polluted or healthy</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
