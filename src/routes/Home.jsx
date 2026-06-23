3ee1124508d1ae0132cf214ab1692ddf293b2783
david seol <dseol@davids-air-2.lan>
2026-06-22 23:59:39 -0400

    Update contact hours

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -158,7 +158,7 @@ export default function Home() {
         <p>9066 Baltimore Ave Unit A, College Park, MD 20740, USA</p>
         <p>Phone: (478) 241-0365</p>
         <p>Email: praxiscenteredu@gmail.com</p>
-        <p>Opening Hours: Mon–Fri 9am–6pm · Sat 10am–2pm · Sun Closed</p>
+        <p>Opening Hours: Mon-Sat 12pm to 8 pm · Sun Closed (closed on July 4th)</p>
       </section>
     </div>
   )
